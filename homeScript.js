404be52a5eca54518ad110e9667e6997d620c3bc
hiro0209 <budouraamen0209@gmail.com>
2017-06-05 23:08:18 +0900

    Add files via upload

--- homeScript.js
+++ homeScript.js
@@ -1,8 +1,16 @@
+var modder={
+	"nikujaga":false,
+	"yuyu":false,
+	"genboo":false
+};
+var name=Object.keys(modder);
+
 function changeTabs(tabName){
 	document.getElementById("top").style.display="none";
 	document.getElementById("overview").style.display="none";
 	document.getElementById("rule").style.display="none";
 	document.getElementById("progress").style.display="none";
+	document.getElementById("modlist").style.display="none";
 	document.getElementById("other").style.display="none";
 	document.getElementById(tabName).style.display="block";
 }
@@ -11,4 +19,41 @@ function selectHeaderImage(){
 	let random=Math.floor(Math.random()*8)+1;
 	let path='image/header_'+random+'.png';
 	document.write('<img src="'+path+'">');
+}
+
+function showModList(listName){
+	document.getElementById(listName).style.display="block";
+	for(let i=0;i<=name.length;i++){
+		if(modder[name[i]]==listName){
+			modder[name[i]]=true;
+			break;
+		}
+	}
+}
+
+function deleteModList(listName){
+	document.getElementById(listName).style.display="none";
+	for(let i=0;i<=name.length;i++){
+		if(modder[name[i]]==listName){
+			modder[name[i]]=false;
+			break;
+		}
+	}
+}
+
+function modListAction(listName,buttonName){
+	let value=document.getElementById(buttonName).value;
+	if(value=="＋"){
+		document.getElementById(buttonName).value="－";
+		showModList(listName);
+	}else if(value=="－"){
+		document.getElementById(buttonName).value="＋";
+		deleteModList(listName);
+	}
+}
+
+function modListAllDelete(){
+	document.getElementById("nikujaga_list").style.display="none";
+	document.getElementById("yuyu_list").style.display="none";
+	document.getElementById("genboo_list").style.display="none";
 }
