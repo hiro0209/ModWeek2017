d9cdea899ab8bf85ed33d0ba3bddfca449fdf808
hiro0209 <budouraamen0209@gmail.com>
2017-06-17 19:59:04 +0900

    Add files via upload

--- homeScript.js
+++ homeScript.js
@@ -36,12 +36,14 @@ function modListAllDelete(){
 }
 
 function getDevice(){
-	var ua = navigator.userAgent;
-	if(ua.indexOf("iPhone")>0||ua.indexOf("iPod")>0||ua.indexOf("Android")>0&&ua.indexOf("Mobile")>0){
-		return "smart";
-	}else{
-		return "other";
-	}
+	$(function(){
+		var ua = navigator.userAgent;
+		if(ua.indexOf("iPhone")>0||ua.indexOf("iPod")>0||ua.indexOf("Android")>0&&ua.indexOf("Mobile")>0){
+			return "smart";
+		}else{
+			return "other";
+		}
+	})
 }
 
 alert(getDevice());
