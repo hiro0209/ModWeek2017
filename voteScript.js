881b8a0e641665bd1bd4c63b702054633f68edc4
hiro0209 <budouraamen0209@gmail.com>
2017-07-17 11:23:10 +0900

    Add files via upload

--- voteScript.js
+++ voteScript.js
@@ -1,19 +1,6 @@
 firebase.initializeApp(config);
 const storage = firebase.database();
 
-function changeContent(id) {
-	document.getElementByd("top-content").style.display = "none";
-	document.getElementById("warning-content").style.display = "none";
-	document.getElementById("vote-content").style.display = "none";
-	document.getElementById(id).style.display = "block";
-}
-
-function resetContent() {
-	document.getElementById("top-content").style.display = "block";
-	document.getElementById("warning-content").style.display = "none";
-	document.getElementById("vote-content").style.display = "none";
-}
-
 function uploadVoteData() {
 	const databaseRef = database.ref("users/" + dviceId);
 	databaseRef.set({
