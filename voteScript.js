a74c4a3313c7d7b7f1a05dfc78d93dd7329db873
hiro0209 <budouraamen0209@gmail.com>
2017-07-17 10:05:23 +0900

    Add files via upload

--- voteScript.js
+++ voteScript.js
@@ -1,10 +1,28 @@
+firebase.initializeApp(config);
+const storage = firebase.database();
+let deviceId = null;
+
+window.addEventListener("load", function()	{
+	document.addEventListener("deviceready", function()	{
+		const uuid = window.device.uuid;
+		document.write(uuid);
+	}, false);
+}, true);
+
 function changeContent(id) {
-	document.getElementById("top-content").style.display = "none";
-	document.getElementById("login-content").style.display = "none";
+	document.getElementByd("top-content").style.display = "none";
+	document.getElementById("warning-content").style.display = "none";
 	document.getElementById(id).style.display = "block";
 }
 
 function resetContent() {
 	document.getElementById("top-content").style.display = "block";
-	document.getElementById("login-content").style.display = "none";
+	document.getElementById("warning-content").style.display = "none";
+}
+
+function uploadVoteData() {
+	const databaseRef = database.ref("users/" + dviceId);
+	databaseRef.set({
+		deviceId: deviceId
+	});
 }
